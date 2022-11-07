f398b6120fc79bb2d753d73539ead3eb08fec61d
Vaughan <vaughan.chase@stacksports.com>
2022-11-06 16:03:59 -0800

    Explore finish

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return localStorage['recipes'];
 }
 
 /**
@@ -35,10 +36,17 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainEl = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  let recipesArr = JSON.parse(recipes);
+  for(let i=0; i < recipesArr.length; ++i){
+    let recipecard = document.createElement('recipe-card');
+    recipecard.data = recipesArr[i]
+    mainEl.append(recipecard);
+  }
 }
 
 /**
@@ -51,6 +59,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+
 }
 
 /**
