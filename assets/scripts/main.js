9375ad87edff1131168b61f0f9126db18624a48e
linda89821 <77847533+linda89821@users.noreply.github.com>
2022-11-07 10:02:21 +0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,7 +10,7 @@ function init() {
   // Add each recipe to the <main> element
   addRecipesToDocument(recipes);
   // Add the event listeners to the form elements
-  //initFormHandler();
+  initFormHandler();
 }
 
 /**
@@ -48,14 +48,7 @@ function addRecipesToDocument(recipes) {
   //            Append each element to <main>
   for (let i = 0; i < recipes.length; i++) {
     let rec = document.createElement("recipe-card");
-    let img_rate = "/assets/images/icons/"+recipes[i]['rating']+"-star.svg";
-    let img = "<img src=" + recipes[i]['imgSrc'] + " alt=" + recipes[i]['imgAlt'] + ">";
-    let title = "<p class='title'> <a href=" + recipes[i]['titleLnk'] + ">" + recipes[i]['titleTxt'] + "</a></p>";
-    let organization = "<p class='organization'>" + recipes[i]['organization'] + "</p>";
-    let rating = "<div class=\"rating\"> <span>" + recipes[i]['rating'] + "</span>" + "<img src=" + img_rate + " alt=" + recipes[i]['rate'] + " stars><span>(" + recipes[i]['numRatings'] + ")</span></div>";
-    let t = "<time>" + recipes[i]['lengthTime'] + "min</time>";
-    let ing = "<p class='ingredients'>" + recipes[i]['ingredients'] + "</p>";
-    rec.shadowRoot.querySelector("article").innerHTML = img + title + organization + rating + t + ing;
+    rec.data = recipes[i];
     ma.appendChild(rec);
   }
 }
@@ -82,26 +75,25 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
-  const form = document.createElement("form");
+  const form = document.getElementById("new-recipe");
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
   form.addEventListener("submit", function(event){
-    event.preventDefault();
-
     // Steps B4-B9 will occur inside the event listener from step B3
     // B4. TODO - Create a new FormData object from the <form> element reference above
-    const formElement = document.querySelector("form");
-    const formData = new FormData(formElement);
+    const formData = new FormData(form);
     // B5. TODO - Create an empty object (I'll refer to this object as recipeObject to
     //            make this easier to read), and then extract the keys and corresponding
     //            values from the FormData object and insert them into recipeObject
-    let recipeObject = new Object();
-    
-    recipeObject = [formData.keys(), formData.values()];
+    recipeObject = {};
+    for(key in formData){
+      recipeObject[key] = formData[key];
+    }
+
     // B6. TODO - Create a new <recipe-card> element
     let recipe_card = document.createElement("recipe-card");
     // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-    recipe_card.innerHtml = recipeObject.data;
+    recipe_card.data = recipeObject;
     // B8. TODO - Append this new <recipe-card> to <main>
     document.querySelector("main").appendChild(recipe_card);
     // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
@@ -109,21 +101,23 @@ function initFormHandler() {
     let res = getRecipesFromStorage();
     res.push(recipeObject);
     saveRecipesToStorage(res);
+    event.preventDefault();
   });
   // B10. TODO - Get a reference to the "Clear Local Storage" button
-  const clear = document.createElement("clear_button");
+  const clear = document.querySelector("danger");
   // B11. TODO - Add a click event listener to clear local storage button
-  clear[0].addEventListener("click", function(event){
-    // Steps B12 & B13 will occur inside the event listener from step B11
-    // B12. TODO - Clear the local storage
-    localStorage.clear();
-    // B13. TODO - Delete the contents of <main>
-    var e = document.querySelector("main");
-        var child = e.lastElementChild; 
-        while (child) {
-            e.removeChild(child);
-            child = e.lastElementChild;
-        }
-  });
+  if(clear){
+    clear.addEventListener("click", function(event){
+      // Steps B12 & B13 will occur inside the event listener from step B11
+      // B12. TODO - Clear the local storage
+      localStorage.clear();
+      // B13. TODO - Delete the contents of <main>
+      var e = document.querySelector("main");
+          var child = e.lastElementChild; 
+          while (child) {
+              e.removeChild(child);
+              child = e.lastElementChild;
+          }
+    });
+  }
 }
-
